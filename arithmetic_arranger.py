f6fb4ca5eba60a447fbeed9aa74118f879240e7b
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2023-04-22 15:32:13 -0500

    Add docstrings

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,5 @@
 def arithmetic_arranger(problems: list, ans: bool= False) -> str:
+    """Return a string with the problems arranged vertically and side-by-side."""
     if len(problems) > 5:
         return 'Error: Too many problems.'
     
@@ -17,6 +18,7 @@ def arithmetic_arranger(problems: list, ans: bool= False) -> str:
     return problems_arranger(problems, dict_lines, ans)
 
 def check_errors(num1: str, operator: str, num2: str):
+    """Check for errors in the input."""
     if operator not in "+-":
         return "Error: Operator must be '+' or '-'."        
     if len(num1) > 4 or len(num2) > 4:
@@ -25,6 +27,17 @@ def check_errors(num1: str, operator: str, num2: str):
         return 'Error: Numbers must only contain digits.'
 
 def problem_solver(num1: str, operator: str, num2: str, answer: str) -> list:
+    """Create a list of strings with the problem solved.
+
+    Args:
+    num1 -- first number
+    operator -- operator
+    num2 -- second number
+    answer -- answer to the problem
+
+    Returns:
+    list of strings with the problem solved
+    """
     if len(num1) < len(num2):
         total_length = len(num2) + 2
     else:
@@ -37,6 +50,16 @@ def problem_solver(num1: str, operator: str, num2: str, answer: str) -> list:
     return [first_line, second_line, dashes, answer]
 
 def problems_arranger(problems: list, dict_lines: dict, ans: bool) -> str:
+    """Arrange the problems vertically and side-by-side.
+
+    Args:
+    problems -- list of problems to be arranged
+    dict_lines -- dictionary with the problems solved
+    ans -- boolean to show or not the answers
+
+    Returns 
+    string with the problems arranged vertically and side-by-side
+    """
     arranged_problems = ''
     for line in range(4):
         for problem in range(len(problems)):
